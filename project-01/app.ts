2e37dc04de626ef0bac3a0f45ae7ac06b3cb5961
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-19 17:38:41 +0530

    logical error case

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,1 +1,10 @@
-console.log("Basic app ts file")
+function add(n1, n2) {
+  return n1+n2;
+}
+
+const number1 = "5";
+const number2 = 2.5;
+
+const result = add(number1, number2);
+
+console.log({ result})
