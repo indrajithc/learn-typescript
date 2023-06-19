62694b38aeeaf506c9a4b3298946d8fafd75022b
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-19 17:52:14 +0530

    passed a phrase value

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,13 +1,15 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(phrase + result);
   } else {
-    return n1 + n2;
+    return result;
   }
 }
 
 const number1 = "5";
 const number2 = 2.5;
 const printResult = true;
+const parsePhrase = "Result is: ";
 
-add(+number1, +number2, printResult);
+add(+number1, +number2, printResult, parsePhrase);
