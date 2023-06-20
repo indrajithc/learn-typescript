abfcb3bd004a601956badc78f47c590446e3c8b1
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 11:44:41 +0530

    nested object

--- project-01/app.ts
+++ project-01/app.ts
@@ -7,3 +7,37 @@ const person: {
 };
 
 console.log(person);
+
+// for a nested object
+
+/**
+ * const product = {
+ *  id: "id",
+ * price: 120.5,
+ * tags: ["grate-offer", "hot-and-new"],
+ * details : {
+ *  title: "Red carpet",
+ *  description: "A great carpet - almost brand-new!"
+ * }
+ * }
+ */
+
+const product: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+} = {
+  id: "id",
+  price: 120.5,
+  tags: ["grate-offer", "hot-and-new"],
+  details: {
+    title: "Red carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
+
+console.log(product);
