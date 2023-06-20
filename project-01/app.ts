5b2b1eb7ef05ba5827fb1b1f9375e362b6171583
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 11:39:43 +0530

    basic object in ts

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,16 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Indran",
+  age: 27,
+};
 
-let number1: number;
-number1 = 4;
-const number2 = 2.5;
-const printResult = true;
-const parsePhrase = "Result is: ";
-
-add(+number1, +number2, printResult, parsePhrase);
+console.log(person);
