63eb64290645316b72f5b43e6508de2849c11af1
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 16:10:14 +0530

    void and undefined

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,28 +1,17 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor,
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, "as-number");
-console.log(combineAges);
+// void is the return type of the function
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combineAsNumberAges = combine("30", "26", "as-number");
-console.log(combineAsNumberAges);
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
 
-const combineNames = combine("Max", "Ann", "as-text");
-console.log(combineNames);
+printResult(add(5, 12));
+
+// let someValue: undefined;
