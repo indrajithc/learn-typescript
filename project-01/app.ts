46d211ea5e5a97d38e0d59914e15b7f181873613
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 15:27:31 +0530

    literals types

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text",
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Max", "Ann");
+const combineAsNumberAges = combine("30", "26", "as-number");
+console.log(combineAsNumberAges);
+
+const combineNames = combine("Max", "Ann", "as-text");
 console.log(combineNames);
