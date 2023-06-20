a3d8795562860ab3e6d4d04b5c69c13b7045511b
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 16:24:42 +0530

    unknown type

--- project-01/app.ts
+++ project-01/app.ts
@@ -1,35 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 4;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-// void is the return type of the function
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-// function printResult(num: number): undefined {
-//   console.log("Result: " + num);
-//   return;
-// }
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-// let someValue: undefined;
-
-let combineValue: (a: number, b: number) => number;
-
-combineValue = add;
-// combineValue = printResult;
-// is an error to fix this
-// combineValue = 5;
-
-console.log(combineValue(8, 8));
-
-addAndHandle(1, 3, (result) => {
-  console.log(result);
-});
