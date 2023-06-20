fcda3705758dfe8fbf14980613d709a4f31a0d93
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 16:27:47 +0530

    never type

--- project-01/app.ts
+++ project-01/app.ts
@@ -7,3 +7,13 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+// never type  because ; after that nothing wil happen break the code
+function generateError(message: string, code: number): never {
+  throw {
+    message: message,
+    errorCode: code,
+  };
+}
+
+generateError("An error occurred!", 500);
