e920335838516a5569e5a929f16b1fddd89db4be
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-20 12:02:22 +0530

    back up

---
+++ project-01/objects-array-etc.ts
@@ -0,0 +1,34 @@
+enum Role {
+  ADMIN = "Admin",
+  READ_ONLY = "4",
+  AUTHOR = "AUTHOR",
+}
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  // marks as a tuple
+  role: Role;
+} = {
+  name: "Indran",
+  age: 27,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+// any type [should avoid]
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", 23];
+
+for (const hobby of person.hobbies) {
+  // no error on to upper case because its type of array
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("Person is an author");
+}
+if (person.role === Role.ADMIN) {
+  console.log("Person is an admin");
+}
