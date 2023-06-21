ef7fdc3c416a218df8baf1d8360a5bff0a3dd0bf
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-21 16:57:11 +0530

    args restructure

--- project-01/app.ts
+++ project-01/app.ts
@@ -32,3 +32,11 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+console.log(copiedPerson);
+
+const addAll = (...numbers: number[]) =>
+  numbers.reduce((curResult, curValue) => curResult + curValue, 0);
+
+const addNumbers = addAll(1, 3, 4, 5, 6, 7);
+console.log({ addNumbers });
