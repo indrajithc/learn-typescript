2583055f042d2ff5f0d5bcba27a01e7f064df32e
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-21 16:40:34 +0530

    error function

---
+++ project-01/app.ts
@@ -0,0 +1,19 @@
+// error functions
+
+const addTwoNumbers = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+console.log(addTwoNumbers(3, 4));
+
+// const printOutput = (output: string | number) => console.log(output);
+const printOutput: (output: string | number) => void = (output) =>
+  console.log(output);
+
+printOutput(addTwoNumbers(3, 44));
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
