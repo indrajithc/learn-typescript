b74108d0dd9543cb38841dc1128ab0b119eb7d79
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-26 12:18:58 +0530

    Simple classs

--- project-02/app.ts
+++ project-02/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello Indran");
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const account = new Department("Accounting");
+
+console.log({ account });
