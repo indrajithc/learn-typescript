d96d2916a57d8698d24de3c2c636667ce3621234
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 00:27:15 +0530

    joint two types

--- project-03/app.ts
+++ project-03/app.ts
@@ -1,1 +1,19 @@
-console.log("Hi product 3");
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+console.log({ e1 });
