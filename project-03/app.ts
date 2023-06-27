29cfb1af134edf066147a3eee3c5f9333bfdfe4b
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 00:28:38 +0530

    back to example

--- project-03/app.ts
+++ project-03/app.ts
@@ -1,14 +1,14 @@
-interface Admin {
+type Admin = {
   name: string;
   privileges: string[];
-}
+};
 
-interface Employee {
+type Employee = {
   name: string;
   startDate: Date;
-}
+};
 
-interface ElevatedEmployee extends Admin, Employee {}
+type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "Max",
