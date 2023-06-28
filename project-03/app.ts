2d831681db4aec018727246160c3a320109b97f9
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 16:52:31 +0530

    type casting

--- project-03/app.ts
+++ project-03/app.ts
@@ -110,3 +110,13 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 110 });
+
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+
+const userInputElement = document.getElementById(
+  "user-input",
+)! as HTMLInputElement;
+
+userInputElement.value = "Hi there!";
