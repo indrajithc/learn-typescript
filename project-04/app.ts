0efa15a29807afcfb03709edcfa62dce3a986202
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 23:13:06 +0530

    example generics

--- project-04/app.ts
+++ project-04/app.ts
@@ -22,3 +22,17 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 const mergedObj = merge({ name: "Max", hobbies: ["Reading"] }, { age: 30 });
 
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionTex = "Got no value.";
+  if (element.length === 1) {
+    descriptionTex = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionTex = "Got  " + element.length + " elements.";
+  }
+  return [element, descriptionTex];
+}
