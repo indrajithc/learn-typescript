e01afea0a19fb7aece771a6abd568e7d06ae2015
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 15:11:45 +0530

    more on type guards

--- project-03/app.ts
+++ project-03/app.ts
@@ -22,3 +22,62 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return +a + +b;
+}
+
+console.log(add(23, 44));
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+printEmployeeInformation({
+  name: "Manu",
+  startDate: new Date(),
+});
+
+class Car {
+  drive() {
+    console.log("Driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if ("loadCargo" in vehicle) {
+    vehicle.loadCargo(10000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
