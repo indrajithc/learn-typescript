c13674493926f87810df0b4810e25be05a59a47a
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 23:09:13 +0530

    extends added

--- project-04/app.ts
+++ project-04/app.ts
@@ -11,8 +11,8 @@
 //   data.split(" ");
 // });
 
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
 // const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
