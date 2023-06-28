e589c32b0254be74b613a89e1066f44d6782baa3
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 23:08:04 +0530

    generic types

--- project-04/app.ts
+++ project-04/app.ts
@@ -1,12 +1,24 @@
-const names: Array<string> = []; // string // generic type
-// names[0].split(" ");
+// const names: Array<string> = []; // string // generic type
+// // names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+
+// const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
+//   { name: "Max", hobbies: ["Reading"] },
+//   { age: 30 },
+// );
+const mergedObj = merge({ name: "Max", hobbies: ["Reading"] }, { age: 30 });
+
+console.log(mergedObj.age);
