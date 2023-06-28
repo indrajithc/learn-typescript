096de4eb1b5a91e9fffe2728744baff4b6f7764a
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 16:28:43 +0530

    discriminated unions

--- project-03/app.ts
+++ project-03/app.ts
@@ -81,3 +81,32 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated unions
+
+interface Bird {
+  type: "bird"; // type assignment
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 110 });
