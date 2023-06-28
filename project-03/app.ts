c5c5ad76d55a3fd04c147de592abd00ee4fbbb30
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 15:12:49 +0530

    better way

--- project-03/app.ts
+++ project-03/app.ts
@@ -74,7 +74,7 @@ const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
   vehicle.drive();
-  if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
     vehicle.loadCargo(10000);
   }
 }
