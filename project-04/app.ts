5618cf56ce8029e6cf929359c926924d27d138f6
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 22:07:10 +0530

    generic type

--- project-04/app.ts
+++ project-04/app.ts
@@ -1,1 +1,12 @@
-console.log("Hi User");
+const names: Array<string> = []; // string // generic type
+// names[0].split(" ");
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+  }, 2000);
+});
+
+promise.then((data) => {
+  data.split(" ");
+});
