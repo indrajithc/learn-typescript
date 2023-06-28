3937618286e403be57691702d5f635035e28a2bb
Indrajith C <indrajith.chandran@mozanta.com>
2023-06-28 17:04:24 +0530

    function overloads

--- project-03/app.ts
+++ project-03/app.ts
@@ -23,6 +23,8 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
