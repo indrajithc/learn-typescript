97c3b11d9657264942c8ae40979fef7d9a0081f5
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-02 20:46:05 +0530

    logger decorator

--- project-05/app.ts
+++ project-05/app.ts
@@ -1,1 +1,19 @@
 // Decorators
+
+function Logger(constructor: Function, t: object) {
+  console.log("Logging ....");
+  console.log(constructor, t);
+}
+
+@Logger
+class Person {
+  name = "Max";
+
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
+
+const person = new Person();
+
+console.log(person);
