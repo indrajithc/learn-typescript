3d54f2e26b213e344984a0e83b084577681ddd5a
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-02 21:12:57 +0530

    property decorator

--- project-05/app.ts
+++ project-05/app.ts
@@ -35,3 +35,33 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+// ---
+
+function Log(_: any, propertyName: any) {
+  console.log("Property decorator!");
+  console.log(propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
