5b344debd9c7f49796f1994f79cd27f0310caf0c
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-09 20:58:31 +0530

    project issue

--- project-06-task/src/app.ts
+++ project-06-task/src/app.ts
@@ -135,7 +135,14 @@ class ProjectList {
     this.element.id = `${type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
