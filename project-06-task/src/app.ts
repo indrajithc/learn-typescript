c0937fb1d754651b6f442185c56803faffd694cd
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-09 19:45:46 +0530

    autobind added

--- project-06-task/src/app.ts
+++ project-06-task/src/app.ts
@@ -1,7 +1,23 @@
+// autobind decorator
+function autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,7 +31,29 @@ class ProjectInput {
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title",
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description",
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people",
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log({ d: this.titleInputElement.value });
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
