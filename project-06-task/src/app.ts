fbe141228dd9815e264819da27811dbbc6714ecf
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 11:00:49 +0530

    drop logic

--- project-06-task/src/app.ts
+++ project-06-task/src/app.ts
@@ -196,7 +196,10 @@ class ProjectItem
   }
 
   @autobind
-  dragStartHandler(_: DragEvent): void {}
+  dragStartHandler(event: DragEvent): void {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
 
   @autobind
   dragEndHandler(_: DragEvent): void {}
@@ -229,12 +232,17 @@ class ProjectList
   }
 
   @autobind
-  dragOverHandler(_: DragEvent): void {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent): void {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
-  dropHandler(_: DragEvent): void {}
+  dropHandler(event: DragEvent): void {
+    console.log(event.dataTransfer!.getData("text/plain"));
+  }
 
   @autobind
   dragLeaveHandler(_: DragEvent): void {
