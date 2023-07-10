94940cfa0b31deef726cf816dc7d610b94cd22a0
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 13:28:26 +0530

    updated

--- project-06-task/webpack.config.js
+++ project-06-task/webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
-  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
