ba954ab81a0dbb3a32aa777449f48d2795cb6de2
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 17:02:54 +0530

    delete added

--- project-09-react-ts/src/components/TodoList.tsx
+++ project-09-react-ts/src/components/TodoList.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 interface TodoListProps {
-  items: { id:string, text: string}[]
+  items: { id:string, text: string}[];
+  onDeleteTodo: (id:string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
 
-const {items } = props;
+const {items, onDeleteTodo } = props;
   return (
     <ul>
       {
         items.map(todo=>(
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}><span>{todo.text}</span>
+          <button type="button" onClick={onDeleteTodo.bind(null, todo.id)}>X</button>
+          </li>
         ))
       }
     </ul>
