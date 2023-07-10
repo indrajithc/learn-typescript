6c4bb0dd56637da6387dccdc52a8e558bc59bc00
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 16:53:16 +0530

    with use state

--- project-09-react-ts/src/App.tsx
+++ project-09-react-ts/src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState} from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
 const  App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course"}]
+  
+const [todos, setTodos] = useState<{id: string; text:string}[]>([])  
 
   const todoAddHandler = (text: string) => {
-    console.log(text)
+    setTodos([
+      {id: Math.random().toString(), text}
+    ])
   }
 
   return (
