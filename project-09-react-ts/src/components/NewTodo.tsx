c91e13dda6e7ca96cb4bf0829af9bb0cf0fb0a38
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 16:48:24 +0530

    add new todo component

---
+++ project-09-react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, { useRef} from "react";
+
+interface NewTodoProps {}
+
+const NewTodo: React.FC<NewTodoProps> = () => {
+
+const textInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    console.log(enteredText)
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef}/>
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
