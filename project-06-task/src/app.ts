2ee89189277e908447941eacf9460f4c286328ea
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 11:12:48 +0530

    drag and drop logic

--- project-06-task/src/app.ts
+++ project-06-task/src/app.ts
@@ -62,13 +62,25 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active,
     );
     this.projects.push(newProject);
-    for (const listenerFn of this.listeners) {
-      listenerFn(this.projects.slice());
-    }
+    this.updateListers();
   }
 
   addListener(listenerFn: Listener<Project>) {
     this.listeners.push(listenerFn);
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project) {
+      project.status = newStatus;
+      this.updateListers();
+    }
+  }
+
+  private updateListers() {
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
   }
 }
 
@@ -240,8 +252,13 @@ class ProjectList
     }
   }
 
+  @autobind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData("text/plain"));
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(
+      prjId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished,
+    );
   }
 
   @autobind
