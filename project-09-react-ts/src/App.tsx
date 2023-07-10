3825dff58eebf11208e568bc82bf25fd1a063a30
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 16:41:48 +0530

    todos as props

--- project-09-react-ts/src/App.tsx
+++ project-09-react-ts/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const  App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course"}]
+
   return (
     <div className="App">
-    Hello
+      {/*  A component that adds todos */}
+    <TodoList items={todos}/>
     </div>
   );
 }
