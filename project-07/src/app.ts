c4d0d3a4a55b92b1b501288e231a84c7586002d3
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-10 15:51:24 +0530

    validation example

--- project-07/src/app.ts
+++ project-07/src/app.ts
@@ -1,12 +1,21 @@
 import { plainToClass } from "class-transformer";
 import "reflect-metadata";
 import { Product } from "./product.model";
+import { validate } from "class-validator";
 
 const products = [
   { title: "A Carpet", price: 29.44 },
   { title: "A Book", price: 10.44 },
 ];
 
+const p1 = new Product("", 3.22);
+validate(p1).then((errors) => {
+  if (errors.length > 0) {
+    console.log(errors);
+  } else {
+    console.log(p1.getInformation());
+  }
+});
 // const p1 = new Product("A Book", 3.22);
 
 // const loadedProducts = products.map((prod) => {
